69bd3a93f5367029914b058a5ca8d14d19ba8988
LiddulBOFH <13317534+LiddulBOFH@users.noreply.github.com>
2022-07-14 18:26:27 -0400

    Re-added another missing weapon
    Accidentally overwrote this one prior to initial commit, it is back now.

--- lua/weapons/weapon_acf_c96.lua
+++ lua/weapons/weapon_acf_c96.lua
@@ -4,18 +4,18 @@ include("weapon_acf_base.lua")
 
 
 SWEP.Base                   = "weapon_acf_base"
-SWEP.PrintName              = "ACF P38"
+SWEP.PrintName              = "ACF C96"
 
 SWEP.IconOffset				= Vector(0,0,0)
 SWEP.IconAngOffset			= Angle()
 
 SWEP.UseHands               = false
-SWEP.ViewModel              = "models/weapons/p38/v_p38.mdl"
+SWEP.ViewModel              = "models/weapons/c96/v_c96.mdl"
 SWEP.ViewModelFlip          = false
 SWEP.ViewModelFOV			= 55
 
 SWEP.ShotSound				= Sound("Weapon_C96.Shoot") -- wrong one, I know, but the normal sound is shitty
-SWEP.WorldModel             = "models/weapons/p38/w_p38.mdl"
+SWEP.WorldModel             = "models/weapons/c96/w_c96.mdl"
 SWEP.HoldType               = "pistol"
 
 SWEP.Weight                 = 1
@@ -30,25 +30,25 @@ SWEP.m_WeaponDeploySpeed    = 1.5
 SWEP.Spread                 = 1
 SWEP.RecoilMod              = 1.5
 
-SWEP.Primary.ClipSize       = 8
-SWEP.Primary.DefaultClip    = 8
+SWEP.Primary.ClipSize       = 20
+SWEP.Primary.DefaultClip    = 20
 SWEP.Primary.Ammo           = "Pistol"
-SWEP.Primary.Automatic      = false
-SWEP.Primary.Delay          = 0.13
+SWEP.Primary.Automatic      = true
+SWEP.Primary.Delay          = 0.07
 
 SWEP.CalcDistance			= 25
 SWEP.CalcDistance2			= 50
 
-SWEP.Caliber                = 9 -- mm diameter of bullet
-SWEP.ACFProjMass            = 0.00814 -- kg of projectile
+SWEP.Caliber                = 7.63 -- mm diameter of bullet
+SWEP.ACFProjMass            = 0.0056 -- kg of projectile
 SWEP.ACFType                = "AP"
-SWEP.ACFMuzzleVel           = 320 -- m/s of bullet leaving the barrel
+SWEP.ACFMuzzleVel           = 425 -- m/s of bullet leaving the barrel
 SWEP.Tracer                 = 0
 
 SWEP.IronScale              = 0
 SWEP.NextIronToggle         = 0
-SWEP.IronSightPos           = Vector(-5.68,-4,4)
-SWEP.IronSightAng           = Angle(0,0,0)
+SWEP.IronSightPos           = Vector(-6.3,-4,3.2)
+SWEP.IronSightAng           = Angle(0,-0.2,0)
 
 SWEP.SprintAng				= Angle(-5,10,0) -- The angle the viewmodel turns to when the player is sprinting
 
