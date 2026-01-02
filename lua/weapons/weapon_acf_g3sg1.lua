342f962c39d0b6e41811e3f9f0e5f86b5cb2f402
LiddulBOFH <13317534+LiddulBOFH@users.noreply.github.com>
2026-01-02 12:07:00 -0600

    Make G3SG1 an actual DMR and not be like CS:S
    This was never actually automatic in real life despite the trigger group retaining the feature, so lets reflect that, and change the slot back to a sniper rifle

--- lua/weapons/weapon_acf_g3sg1.lua
+++ lua/weapons/weapon_acf_g3sg1.lua
@@ -12,7 +12,7 @@ SWEP.ShotSound				= Sound(")weapons/g3sg1/g3sg1-1.wav")
 SWEP.WorldModel             = "models/weapons/w_snip_g3sg1.mdl"
 SWEP.HoldType               = "ar2"
 
-SWEP.Slot                   = 2
+SWEP.Slot                   = 3
 SWEP.SlotPos                = 0
 
 SWEP.Spawnable              = true
@@ -25,9 +25,9 @@ SWEP.RecoilMod              = 1.5
 SWEP.Primary.ClipSize       = 20
 SWEP.Primary.DefaultClip    = 20
 SWEP.Primary.Ammo           = "AR2"
-SWEP.Primary.Automatic      = true
+SWEP.Primary.Automatic      = false
 SWEP.Primary.Delay          = 0.1
-SWEP.FiremodeSetting		= 2
+SWEP.FiremodeSetting		= 1
 
 SWEP.Caliber                = 7.62 -- mm diameter of bullet
 SWEP.ACFProjMass            = 0.011 -- kg of projectile
